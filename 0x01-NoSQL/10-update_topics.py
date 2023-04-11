673cf832c22454baecf40724dfcb1cad6bf894e1
SifisoSithole <103381999+SifisoSithole@users.noreply.github.com>
2023-04-11 12:22:26 +0000

    task 10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+This module contains the `update_topics`
+"""
+
+def update_topics(mongo_collection, name, topics):
+    """
+    changes all topics of a school document based on the name
+    args:
+        mongo_collection (Collection): pymongo collection object
+        name (str): will be the school name to update
+        topics (list): list of topics approached in the school
+    """
+    if not mongo_collection or len(topics) == 0:
+        return
+    mongo_collection.update_many({"name": name}, {$set: {"topics": topics}})
